bf31e865a1354a7d92ddbeed7f1944201908aea9
Vale <vale.v@me.com>
2022-08-27 08:27:05 +0200

    Restoring download at creation time

--- MacMulator/RunVM/VirtualizationFrameworkInstallVMViewController.swift
+++ MacMulator/RunVM/VirtualizationFrameworkInstallVMViewController.swift
@@ -50,56 +50,50 @@ class VirtualizationFrameworkInstallVMViewController: NSViewController {
     
     override func viewDidAppear() {
         if let virtualMachine = virtualMachine {
-            var restoreImage: URL
-            if restoreImageURL != nil {
-                restoreImage = restoreImageURL!
-            } else {
-                // download
-                restoreImage = URL(fileURLWithPath: "aaa")
-            }
-            
-            DispatchQueue.main.async {
-                let installer = VZMacOSInstaller(virtualMachine: virtualMachine, restoringFromImageAt: restoreImage)
-                
-                print("Starting installation.")
-                installer.install(completionHandler: { (result: Result<Void, Error>) in
-                    if case let .failure(error) = result {
-                        print("Installation failed with error: " + error.localizedDescription)
-                    } else {
-                        print("Installation succeeded.")
-                    }
+            if let restoreImageURL = restoreImageURL {
+                DispatchQueue.main.async {
+                    let installer = VZMacOSInstaller(virtualMachine: virtualMachine, restoringFromImageAt: restoreImageURL)
                     
-                    virtualMachine.stop(completionHandler: { err in
-                        self.dismiss(self)
-                        self.parentRunner?.instllationComplete(result)
+                    print("Starting installation.")
+                    installer.install(completionHandler: { (result: Result<Void, Error>) in
+                        if case let .failure(error) = result {
+                            print("Installation failed with error: " + error.localizedDescription)
+                        } else {
+                            print("Installation succeeded.")
+                        }
+                        
+                        virtualMachine.stop(completionHandler: { err in
+                            self.dismiss(self)
+                            self.parentRunner?.instllationComplete(result)
+                        })
                     })
-                })
-                
-                let startTime = Int64(Date().timeIntervalSince1970)
-                Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
                     
-                    _ = installer.progress.observe(\.fractionCompleted, options: [.initial, .new]) { (progress, change) in
-                        self.progress = change.newValue! * 100
-                        print("Installation progress: \(self.progress).")
-                    }
-                    
-                    
-                    let currentValue = self.progressBar.doubleValue
-                    if (currentValue <= 0) {
-                        self.descriptionLabel.stringValue = "Installing macOS on the Virtual Machine. The process will start in a moment..."
-                        self.estimateTimeRemainingLabel.stringValue = "Estimate time remaining: Calculating..."
-                    } else {
-                        self.descriptionLabel.stringValue = "Installing macOS on the Virtual Machine (" + String(Int(self.progress)) + "%)..."
-                        self.estimateTimeRemainingLabel.stringValue = "Estimate time remaining: " + Utils.computeTimeRemaining(startTime: startTime, progress: self.progress)
-                    }
-                    if (self.progress > currentValue) {
-                        let delta = self.progress - currentValue;
-                        self.progressBar.increment(by: delta)
-                    }
-                    if (self.isComplete()) {
-                        timer.invalidate()
-                    }
-                });
+                    let startTime = Int64(Date().timeIntervalSince1970)
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+                        
+                        _ = installer.progress.observe(\.fractionCompleted, options: [.initial, .new]) { (progress, change) in
+                            self.progress = change.newValue! * 100
+                            print("Installation progress: \(self.progress).")
+                        }
+                        
+                        
+                        let currentValue = self.progressBar.doubleValue
+                        if (currentValue <= 0) {
+                            self.descriptionLabel.stringValue = "Installing macOS on the Virtual Machine. The process will start in a moment..."
+                            self.estimateTimeRemainingLabel.stringValue = "Estimate time remaining: Calculating..."
+                        } else {
+                            self.descriptionLabel.stringValue = "Installing macOS on the Virtual Machine (" + String(Int(self.progress)) + "%)..."
+                            self.estimateTimeRemainingLabel.stringValue = "Estimate time remaining: " + Utils.computeTimeRemaining(startTime: startTime, progress: self.progress)
+                        }
+                        if (self.progress > currentValue) {
+                            let delta = self.progress - currentValue;
+                            self.progressBar.increment(by: delta)
+                        }
+                        if (self.isComplete()) {
+                            timer.invalidate()
+                        }
+                    });
+                }
             }
         }
     }
