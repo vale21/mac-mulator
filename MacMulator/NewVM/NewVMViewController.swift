0ec91bdc77e55d264bd624c67602eeeb65d61fbc
Vale <vale.v@me.com>
2025-05-28 18:28:21 +0200

    Localizing new VM view

--- MacMulator/NewVM/NewVMViewController.swift
+++ MacMulator/NewVM/NewVMViewController.swift
@@ -8,17 +8,26 @@
 import Cocoa
 
 class NewVMViewController: NSViewController, NSComboBoxDataSource, NSComboBoxDelegate, NSTextViewDelegate {
-    @IBOutlet var vmType: NSComboBox!
-    @IBOutlet var vmSubType: NSComboBox!
+    @IBOutlet weak var essentialInformationBox: NSBox!
+    @IBOutlet weak var vmNameLabel: NSTextField!
     @IBOutlet var vmName: NSTextField!
+    @IBOutlet weak var vmTypeLabel: NSTextField!
+    @IBOutlet var vmType: NSComboBox!
+    @IBOutlet weak var vmSubTypeLabel: NSTextField!
+    @IBOutlet var vmSubType: NSComboBox!
     @IBOutlet var vmDescription: NSTextView!
+    @IBOutlet weak var installMediaLabel: NSTextField!
     @IBOutlet var installMedia: NSTextField!
+    @IBOutlet weak var findOSButton: NSButton!
     @IBOutlet var obtainOSButton: NSButton!
     @IBOutlet var fullConfiguration: NSButton!
-
+    @IBOutlet weak var createButton: NSButton!
+    
     var rootController: RootViewController?
 
     static let DESCRIPTION_DEFAULT_MESSAGE = NSLocalizedString("NewVMViewController.defaultMessage", comment: "")
+    
+    var starting: Bool = true
 
     @IBAction func findInstallMedia(_: Any) {
         if vmType.stringValue == QemuConstants.OS_IOS {
@@ -60,7 +69,20 @@ class NewVMViewController: NSViewController, NSComboBoxDataSource, NSComboBoxDel
     }
 
     override func viewWillAppear() {
+        starting = true
         vmSubType.stringValue = QemuConstants.SUB_OTHER_GENERIC
+        
+        essentialInformationBox.title = NSLocalizedString("NewVMController.essentialInformationBox", comment: "")
+        vmNameLabel.stringValue = NSLocalizedString("NewVMController.vmNameLabel", comment: "")
+        vmTypeLabel.stringValue = NSLocalizedString("NewVMController.vmTypeLabel", comment: "")
+        vmSubTypeLabel.stringValue = NSLocalizedString("NewVMController.vmSubTypeLabel", comment: "")
+        vmDescription.string = NewVMViewController.DESCRIPTION_DEFAULT_MESSAGE
+        installMediaLabel.stringValue = NSLocalizedString("NewVMController.installMediaLabel", comment: "")
+        findOSButton.title = NSLocalizedString("NewVMController.findOSButton", comment: "")
+        obtainOSButton.title = NSLocalizedString("NewVMController.obtainOSButton", comment: "")
+        fullConfiguration.title = NSLocalizedString("NewVMController.fullConfiguration", comment: "")
+        createButton.title = NSLocalizedString("NewVMController.createButton", comment: "")
+        starting = false
     }
 
     override func prepare(for segue: NSStoryboardSegue, sender _: Any?) {
@@ -102,7 +124,7 @@ class NewVMViewController: NSViewController, NSComboBoxDataSource, NSComboBoxDel
     }
 
     func textViewDidChangeSelection(_: Notification) {
-        if vmDescription.string == NewVMViewController.DESCRIPTION_DEFAULT_MESSAGE {
+        if !starting && vmDescription.string == NewVMViewController.DESCRIPTION_DEFAULT_MESSAGE {
             vmDescription.string = ""
         }
     }
